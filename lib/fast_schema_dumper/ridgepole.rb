305db7749737608d7d5284cb4513f00c682ea197
moznion <moznion@mail.moznion.net>
2025-12-16 18:23:33 +0900

    Fix warnings from `rake standard`
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- lib/fast_schema_dumper/ridgepole.rb
+++ lib/fast_schema_dumper/ridgepole.rb
@@ -1,4 +1,4 @@
-require_relative './fast_dumper'
+require_relative "fast_dumper"
 
 # Loading this file will overwrite `Ridgepole::Dumper.dump`.
 
@@ -10,11 +10,11 @@ module Ridgepole
     alias_method :original_dump, :dump
 
     def dump
-      case ENV['FAST_SCHEMA_DUMPER_MODE']
-      in 'disabled'
+      case ENV["FAST_SCHEMA_DUMPER_MODE"]
+      in "disabled"
         original_dump
-      in 'verify'
-        puts "Warning: fast_schema_dumper is enabled in verify mode" unless ENV['FAST_SCHEMA_DUMPER_SUPPRESS_MESSAGE'] == '1'
+      in "verify"
+        puts "Warning: fast_schema_dumper is enabled in verify mode" unless ENV["FAST_SCHEMA_DUMPER_SUPPRESS_MESSAGE"] == "1"
         original_results = original_dump
         fast_results = fast_dump
         File.write("orig.txt", original_results)
@@ -24,7 +24,7 @@ module Ridgepole
         end
         fast_results
       else
-        puts "Warning: fast_schema_dumper is enabled" unless ENV['FAST_SCHEMA_DUMPER_SUPPRESS_MESSAGE'] == '1'
+        puts "Warning: fast_schema_dumper is enabled" unless ENV["FAST_SCHEMA_DUMPER_SUPPRESS_MESSAGE"] == "1"
         fast_dump
       end
     end
